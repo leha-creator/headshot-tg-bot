668c9b23eeaa0627c72858c967595cceee9b7129
Alexey Andreev <lehagagog@mail.ru>
2025-03-25 19:43:55 +0400

    Усовершенствование рассылки

--- src/commands/distribute.command.ts
+++ src/commands/distribute.command.ts
@@ -1,7 +1,7 @@
-import { Telegraf } from "telegraf";
-import { Command } from "./command.class";
-import { IBotContext } from "../context/context.interface";
-import { AdminService } from "../helpers/admin.service";
+import {Telegraf} from "telegraf";
+import {Command} from "./command.class";
+import {IBotContext} from "../context/context.interface";
+import {AdminService} from "../helpers/admin.service";
 import {model} from "mongoose";
 import {UserSchema} from "../Models/User.model";
 import {logger} from "../helpers/logger";
@@ -30,20 +30,21 @@ export class DistributeCommand extends Command {
                 let forbidden_counter = 0;
                 let processed_counter = 0;
                 for (const user of users) {
-                    if (total_users > 100) {
-                        if (processed_counter % 100 === 0) {
-                            const percent = Math.floor(processed_counter / total_users * 100);
-                            await this.bot.telegram.editMessageText(admin_chat_id, message.message_id, undefined, "Рассылка запущена (кол-во пользователей: " + total_users + ")\nПрогресс " + percent + "%");
-                        }
-                    }
                     try {
-                        await ctx.telegram.copyMessage(user.chat_id, ctx.message.chat.id, ctx.message.reply_to_message.message_id);
+                        if (total_users > 100) {
+                            if (processed_counter % 100 === 0) {
+                                const percent = Math.round(processed_counter / total_users * 1000) / 10;
+                                await this.bot.telegram.editMessageText(admin_chat_id, message.message_id, undefined, "Рассылка запущена (кол-во пользователей: " + total_users + ")\nПрогресс " + percent + "% (" + processed_counter + ")");
+                            }
+                            await ctx.telegram.copyMessage(user.chat_id, ctx.message.chat.id, ctx.message.reply_to_message.message_id);
+                        }
                     } catch (e) {
                         forbidden_counter += 1;
                         logger.error(e);
                     }
                     processed_counter += 1;
                 }
+
                 await this.bot.telegram.editMessageText(admin_chat_id, message.message_id, undefined, "Рассылка запущена (кол-во пользователей: " + total_users + ")\nПрогресс " + 100 + "%\nРассылка завершена, пользователей заблокировавших бота: " + forbidden_counter);
             }
         });
