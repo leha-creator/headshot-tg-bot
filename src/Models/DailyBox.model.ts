23e73a5a7242a21957c8298c4b8b89618e504e73
Alexey Andreev <lehagagog@mail.ru>
2025-04-16 18:32:18 +0400

    Вырезает локалхост

--- src/Models/DailyBox.model.ts
+++ src/Models/DailyBox.model.ts
@@ -1,23 +1,24 @@
-import { Schema } from 'mongoose';
+import {model, Schema} from 'mongoose';
 
-export interface IDailyBox
-{
+export interface IDailyBox {
     chat_id: number,
     is_success: boolean,
     is_bonus_accrued: boolean
     next_notified: boolean,
-    code: number|string|undefined,
+    code: number | string | undefined,
     box_id: string
 }
 
-export const DailyBoxSchema = new Schema<IDailyBox>
+export const DailyBoxSchema = model("DailyBox", new Schema<IDailyBox>
 ({
-    chat_id: { type: Number, required: true },
-    is_success: { type: Boolean, default: false },
-    is_bonus_accrued: { type: Boolean, default: false },
-    next_notified: { type: Boolean, default: false },
-    code: { type: Number, required: false },
-    box_id: { type: String, required: true },
+    chat_id: {type: Number, required: true},
+    is_success: {type: Boolean, default: false},
+    is_bonus_accrued: {type: Boolean, default: false},
+    next_notified: {type: Boolean, default: false},
+    code: {type: Number, required: false},
+    box_id: {type: String, required: true},
 }, {
     timestamps: true,
-});
+}));
+
+
